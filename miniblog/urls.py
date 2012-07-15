f10e6a25c21828fb00d8ec3f6146a9f33e010fb7
Ramón Medrano Llamas <m3drano@gmail.com>
2012-07-15 18:35:06 +0200

    Add the basic URL structure.

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -12,6 +12,11 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    # URLs for blog engine app.
+    url(r'^$', 'blogengine.views.index', name='index'),
+    url(r'^article/(?P<article_id>\d+)/$', 'blogengine.views.article', name='article'),
+    url(r'^archive/(?P<year>\d+)/(?P<month>\d+)/$', 'blogengine.views.archive', name='archive'),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
