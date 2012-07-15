fc8f1628a7567396d7471a82193914f0e35b561f
Ramón Medrano Llamas <m3drano@gmail.com>
2012-07-15 17:32:50 +0200

    Add initial version of the blog engine models.

--- blogengine/models.py
+++ blogengine/models.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Models module for the blog engine app."""
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category model that allows classify the articles."""
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Article(models.Model):
+    """Article model that stores the text and its metadata."""
+    title = models.CharField(max_length=500)
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    creation_time = models.DateTimeField()
+    last_update_time = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category)
+
+    def __unicode__(self):
+        return '"%s" by %s' % (self.title, self.author.username)
